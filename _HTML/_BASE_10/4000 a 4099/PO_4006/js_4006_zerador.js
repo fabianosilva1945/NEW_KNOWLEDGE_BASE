7fd86826f4351e9fb4637667ac155126cb947b2b
fabiano.silva <redesdetalhada@gmail.com>
2026-01-23 10:01:11 -0300

    pASTA 4000 A 4099
    pASTA 4000 A 4099

---
+++ _HTML/_BASE_10/4000 a 4099/PO_4006/js_4006_zerador.js
@@ -0,0 +1,26 @@
+// Defina o nome do HTML atual (sem extensão)
+const nomeHtml = 'PO_4006'; // Altere para o nome do seu arquivo, ex: 'pagina2', 'pagina3', etc.
+
+// Verifica se há um contador salvo no localStorage para este arquivo específico
+let contador = localStorage.getItem('contador_' + nomeHtml) || 0;
+document.getElementById('contador').innerText = contador;
+
+// Incrementa o contador e salva no localStorage
+contador++;
+localStorage.setItem('contador_' + nomeHtml, contador);
+document.getElementById('contador').innerText = contador;
+
+// Função para resetar o contador
+function resetCounter() {
+    contador = 0;
+    localStorage.setItem('contador_' + nomeHtml, contador);
+    document.getElementById('contador').innerText = contador;
+}
+
+// Adiciona um ouvinte de evento para detectar a combinação de teclas para resetar
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.shiftKey && event.key === 'R') {
+        resetCounter();
+        alert("Contador resetado.");
+    }
+});
